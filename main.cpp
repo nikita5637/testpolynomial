b3b72a3df7043dca583bec231f240ec86490a21b
root <root@work.varavin.su>
2018-02-24 22:29:17 +0300

    Generator was deleted

--- main.cpp
+++ main.cpp
@@ -1,16 +1,6 @@
 #include "polynomial.h"
 #include <iostream>
 
-std::string Generator() {
-	std::string str= "";
-	char alph[] = "01234-+*^56x789";
-	int countSymbols = rand() % 30; //количество символов в выходной строке
-	for ( ; countSymbols; countSymbols--) {
-		str += alph[rand() % sizeof(alph)];
-	}
-	return str;
-}
-
 int main(int argc, const char *argv[]) {
 	extern std::string str;
 	if (argc > 1) {
